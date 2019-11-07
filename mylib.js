169853d00a2e9ca4edee3439dc67ffb594dfd969
Xavier ARTUSI <xavart2011@gmail.com>
2019-11-07 17:48:02 +0100

    dopdown menu

--- mylib.js
+++ mylib.js
@@ -1,5 +1,5 @@
-function load_page(page) {
-    document.getElementById("content").innerHTML = "<iframe  src=" + page + "></iframe>";
+function load_page(page, content) {
+    content.innerHTML = "<iframe  src=" + page + "></iframe>";
 }
 function set_active(href, id) {
     if (id === void 0) { id = 'menu'; }
@@ -8,3 +8,35 @@ function set_active(href, id) {
     var selection = Array.from(menu.children).filter(function (child) { return child.getAttribute("href") == "#" + href; });
     selection.map(function (elem) { return elem.setAttribute('class', 'active'); });
 }
+var Menu = /** @class */ (function () {
+    function Menu(container, menuItems) {
+        this.menuEl = document.createElement('ul');
+        container.appendChild(this.menuEl);
+        this._populate(this.menuEl, menuItems);
+    }
+    Menu.prototype._populate = function (parent, menuItems) {
+        var _loop_1 = function (item) {
+            var itemEl = document.createElement('li');
+            var linkEl = document.createElement('a');
+            linkEl.text = item.title;
+            itemEl.appendChild(linkEl);
+            if (item.action != null)
+                itemEl.addEventListener('click', function () { return item.action(); });
+            if (item.subelems) {
+                var submenu = document.createElement('ul');
+                linkEl.setAttribute("class", "dropbtn");
+                submenu.setAttribute("class", "dropdown-content");
+                itemEl.setAttribute("class", "dropdown");
+                this_1._populate(submenu, item.subelems);
+                itemEl.appendChild(submenu);
+            }
+            parent.appendChild(itemEl);
+        };
+        var this_1 = this;
+        for (var _i = 0, menuItems_1 = menuItems; _i < menuItems_1.length; _i++) {
+            var item = menuItems_1[_i];
+            _loop_1(item);
+        }
+    };
+    return Menu;
+}());
--- mylib.ts
+++ mylib.ts
@@ -1,14 +1,54 @@
 interface ArrayConstructor {
-     from(arrayLike: any, mapFn?, thisArg?): Array<any>;
- }
+  from(arrayLike: any, mapFn?, thisArg?): Array<any>;
+}
 
-function load_page(page: string): void {
-     document.getElementById("content").innerHTML=`<iframe  src=${page}></iframe>`;
+function load_page(page: string, content:HTMLElement): void {
+  content.innerHTML = `<iframe  src=${page}></iframe>`;
 }
 
 function set_active(href: string, id: string = 'menu'): void {
-     const menu = document.getElementById(id)
-     Array.from(menu.children).map((child: any) => child.removeAttribute("class"))
-     const selection = Array.from(menu.children).filter((child: any) => child.getAttribute("href")==`#${href}`)
-     selection.map((elem: any) => elem.setAttribute('class', 'active'))
- }
+  const menu = document.getElementById(id)
+  Array.from(menu.children).map((child: any) => child.removeAttribute("class"))
+  const selection = Array.from(menu.children).filter((child: any) => child.getAttribute("href") == `#${href}`)
+  selection.map((elem: any) => elem.setAttribute('class', 'active'))
+}
+
+type MenuItems = {
+  title: string
+  action: CallableFunction | null
+  subelems?: MenuItems[]
+}
+
+
+class Menu {
+  menuEl: HTMLUListElement
+
+  constructor(container: HTMLElement, menuItems: MenuItems[]) {
+    this.menuEl = document.createElement('ul')
+    container.appendChild(this.menuEl)
+    this._populate(this.menuEl, menuItems)
+  }
+
+  _populate(parent: HTMLUListElement, menuItems: MenuItems[]){
+    for(let item of menuItems){
+      const itemEl: HTMLLIElement = document.createElement('li')
+      const linkEl: HTMLAnchorElement = document.createElement('a')
+      linkEl.text = item.title
+      itemEl.appendChild(linkEl)
+      if(item.action != null)
+        itemEl.addEventListener('click', () => item.action())
+      
+      if(item.subelems){
+        const submenu: HTMLUListElement = document.createElement('ul')
+        linkEl.setAttribute("class", "dropbtn")
+        submenu.setAttribute("class", "dropdown-content")
+        itemEl.setAttribute("class","dropdown")
+        this._populate(submenu, item.subelems)
+        itemEl.appendChild(submenu)
+      }
+      parent.appendChild(itemEl)
+    }
+  }
+}
+
+
