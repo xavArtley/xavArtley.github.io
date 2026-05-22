a46a33eb03483d5e6029ced97aa74723132cce64
Xavier ARTUSI <xavier.artusi@cea.fr>
2026-05-22 16:10:08 +0200

    design improvement

--- mylib.js
+++ mylib.js
@@ -1,37 +1,69 @@
 function load_page(page, content) {
-    content.innerHTML = "<iframe  src=" + page + "></iframe>";
+    content.innerHTML = '<iframe src="' + page + '"></iframe>';
+    // Close mobile nav after navigation
+    var ul  = document.querySelector('.topnav ul');
+    var btn = document.getElementById('hamburger-btn');
+    if (ul)  ul.classList.remove('open');
+    if (btn) btn.classList.remove('open');
+    // Update active link
+    _updateActiveLink();
 }
-var Menu = /** @class */ (function () {
+
+function toggleMobileNav() {
+    var ul  = document.querySelector('.topnav ul');
+    var btn = document.getElementById('hamburger-btn');
+    if (ul)  ul.classList.toggle('open');
+    if (btn) btn.classList.toggle('open');
+}
+
+function _updateActiveLink() {
+    var hash = window.location.hash.replace('#', '');
+    document.querySelectorAll('nav li a').forEach(function(a) {
+        a.classList.remove('active');
+        var href = a.getAttribute('href') || '';
+        if (hash && href === '#' + hash) {
+            a.classList.add('active');
+        }
+    });
+}
+
+// Re-run on hash change (e.g. links from within iframe pages)
+window.addEventListener('hashchange', function() {
+    var hash = window.location.hash.replace('#', '');
+    if (hash) {
+        var content = document.getElementById('content');
+        if (content) load_page(hash, content);
+    }
+    _updateActiveLink();
+});
+
+var Menu = (function () {
     function Menu(container, menuItems) {
         this.menuEl = document.createElement('ul');
         container.appendChild(this.menuEl);
         this._populate(this.menuEl, menuItems);
     }
     Menu.prototype._populate = function (parent, menuItems) {
-        var _loop_1 = function (item) {
+        var self = this;
+        menuItems.forEach(function (item) {
             var itemEl = document.createElement('li');
             var linkEl = document.createElement('a');
-            linkEl.text = item.title;
+            linkEl.textContent = item.title;
             if (item.href != null)
-                linkEl.href = (item.href.slice(0, 1) == '#') ? item.href : '#' + item.href;
+                linkEl.href = (item.href.slice(0, 1) === '#') ? item.href : '#' + item.href;
             itemEl.appendChild(linkEl);
             if (item.action != null)
-                itemEl.addEventListener('click', function () { return item.action(); });
+                itemEl.addEventListener('click', function () { item.action(); });
             if (item.subelems) {
                 var submenu = document.createElement('ul');
-                linkEl.setAttribute("class", "dropbtn");
-                submenu.setAttribute("class", "dropdown-content");
-                itemEl.setAttribute("class", "dropdown");
-                this_1._populate(submenu, item.subelems);
+                linkEl.setAttribute('class', 'dropbtn');
+                submenu.setAttribute('class', 'dropdown-content');
+                itemEl.setAttribute('class', 'dropdown');
+                self._populate(submenu, item.subelems);
                 itemEl.appendChild(submenu);
             }
             parent.appendChild(itemEl);
-        };
-        var this_1 = this;
-        for (var _i = 0, menuItems_1 = menuItems; _i < menuItems_1.length; _i++) {
-            var item = menuItems_1[_i];
-            _loop_1(item);
-        }
+        });
     };
     return Menu;
 }());
